42bb1a530ded4f8ed3dd11d0c6f72df315eeaac3
zbynioo85 <zbynioo85@gmail.com>
2019-12-04 13:26:26 +0100

    add css file, styleing Select

--- src/component/Select.jsx
+++ src/component/Select.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "../style/Select.css";
 const Select = props => {
   return (
-    <>
-      <h1 className="App__text">Wybierz walute:</h1>
-      <select value={props.value} onChange={props.onChange}>
+    <div className="Select">
+      <h1 className="Select__text">Wybierz walute:</h1>
+      <select className="Select__select" value={props.value} onChange={props.onChange}>
         <option value="" disabled>Wybierz walutę</option>
         <option value="USD">Dolar amreykański(USD)</option>
         <option value="CAD">Dolar kanadyjski(CAD)</option>
@@ -12,7 +13,7 @@ const Select = props => {
         <option value="GBP">Funt szterling(GBP)</option>
         <option value="CZK">Korona czeska(CZK)</option>
       </select>
-    </>
+    </div>
   );
 };
 
