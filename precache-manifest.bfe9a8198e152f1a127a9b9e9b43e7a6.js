4e39444547e32396de8ede4d2f3f9503ef2819c2
zbynioo85 <zbynioo85@gmail.com>
2019-12-04 18:28:32 +0100

    Updates

---
+++ precache-manifest.bfe9a8198e152f1a127a9b9e9b43e7a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1c732f96fc8cc4eff43627ba5ed9064",
+    "url": "/CurrencyCalculator/./index.html"
+  },
+  {
+    "revision": "088ba10bf1ce65a56299",
+    "url": "/CurrencyCalculator/./static/css/main.6f4b8424.chunk.css"
+  },
+  {
+    "revision": "22670080652b5cdc0fa9",
+    "url": "/CurrencyCalculator/./static/js/2.29c6d527.chunk.js"
+  },
+  {
+    "revision": "088ba10bf1ce65a56299",
+    "url": "/CurrencyCalculator/./static/js/main.35ae8e02.chunk.js"
+  },
+  {
+    "revision": "0490057c27a43e4d42c6",
+    "url": "/CurrencyCalculator/./static/js/runtime-main.6383d509.js"
+  }
+]);
